35072c5031d41220e3bf2ff6f6780f4c2bb5dd9c
emil237 <marianatalaat1@gmail.com>
2025-07-05 18:33:14 +0300

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
 #
 # Command:
-# wget https://raw.githubusercontent.com/emil237/channel-emil-nabil/main/installer.sh -qO - | /bin/sh
+# wget https://raw.githubusercontent.com/emil237/channel-emil-nabil/refs/heads/main/installer.sh -qO - | /bin/sh
 #
-# ###########################################
+# ###########################################################
 
-MY_URL="https://raw.githubusercontent.com/emil237/channel-emil-nabil/main"
+MY_URL="https://raw.githubusercontent.com/emil237/channel-emil-nabil/refs/heads/main"
 
 echo "******************************************************************************************************************"
 echo "        DOWNLOAD AND INSTALL CHANNEL"
 echo "=================================================================================================================="
+if [ -f /etc/opkg/opkg.conf ]; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+fi
+
+echo "        REMOVE OLD CHANNELS..."
+
+rm -rf /etc/enigma2/lamedb
+rm -rf /etc/enigma2/*list
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
 
 #####################################################################################
-echo "        INSTALLING ASTRA-SM PATCH"
-opkg install astra-sm -y >/dev/null 2>&1
-opkg install dvbsnoop -y >/dev/null 2>&1
-sleep 1
-
-echo "        DOWNLOADING ABERTIS SCRIPT"
-wget -qO /etc/astra/scripts/abertis "https://drive.google.com/uc?id=1B0k60UTtmrHgQTrRkI6SFR3TenCZO_eL&export=download"
-chmod 755 /etc/astra/scripts/abertis
-sleep 1
-
-echo "        DOWNLOADING ASTRA CONFIG"
-wget -qO /etc/astra/astra.conf "https://drive.google.com/uc?id=1B16pdta4o2u_PChUAG1hEAWKj1So2vCh&export=download"
-chmod 755 /etc/astra/astra.conf
-sleep 1
-
-echo ""
-echo "        INSTALLING CHANNEL FILES..."
+echo "        INSTALLING NEW CHANNELS..."
 cd /tmp
 set -e
 wget -q "${MY_URL}/channels_backup_by_Emil-Nabil.tar.gz"
@@ -39,6 +36,10 @@ rm -f /tmp/channels_backup_by_Emil-Nabil.tar.gz
 sleep 2
 
 echo ""
+echo "        INSTALLING ASTRA-SM PATCH"
+opkg install astra-sm -y >/dev/null 2>&1
+sleep 1
+echo ""
 echo ""
 echo "****************************************************************************************************************************"
 echo "#       CHANNEL INSTALLED SUCCESSFULLY       #"
@@ -47,26 +48,18 @@ echo "**************************************************************************
 echo "   UPLOADED BY >>>> EMIL_NABIL"
 sleep 4
 echo "========================================================================================================================="
-
-###########################################
-# Detect system type
-if [ -f /etc/opkg/opkg.conf ]; then
-    OSTYPE="Opensource"
-else
-    OSTYPE="DreamOS"
-fi
-
-# Restart Enigma2 based on system type
-echo "**********************************************************************************"
 echo "        >>>> RESTARTING <<<<"
 echo "**********************************************************************************"
 
-if [ "$OSTYPE" = "Opensource" ]; then
+if [ "${OSTYPE}" = "Opensource" ]; then
     killall -9 enigma2
 else
-    systemctl restart enigma2 || init 6
+    systemctl restart enigma2
 fi
 
 exit 0
+
+
+
 
 
